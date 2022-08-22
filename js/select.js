7274a1bea378d74b9a04b6b362ea953f55cbeafd
atikhasanps <atikhasanps@gmail.com>
2022-08-22 23:00:57 +0600

    third

--- js/select.js
+++ js/select.js
@@ -1,22 +1,40 @@
-const playerArray =[];
+// const playerArray =[];
 
-function displayPlayer() {
+// function displayPlayer(selectPlayer) {
     
-}
+//     // console.log(selectPlayer);
+//     const playerListbody = document.getElementById('player-list');
+
+//     playerListbody.innerHTML = '';
+//     for(let i = 0; i<selectPlayer.length; i++ ){
+
+//         // console.log(playerArray[i]).playerName;
+//         const name = playerArray[i].playerName;
+
+//         const tr = document.createElement("tr");
+//         tr.innerHTML = `
+
+//         <th>${i +1}</th>
+//         <td>${name}</td>
+        
+//         `;
+//         playerListbody.appendChild("ol");
+//     }
+// }
 
 
 
 
-function addToSelect(element){
+// function addToSelect(element){
     
-    const playerName = element.parentNode.parentNode.children[0].innerText;
+//     const playerName = element.parentNode.parentNode.children[0].innerText;
 
-    const playerObj ={
-        playerName : playerName
-    }
-    playerArray.push(playerObj);
+//     const playerObj ={
+//         playerName : playerName
+//     }
+//     playerArray.push(playerObj);
  
-    document.getElementById('player-list').innerText = playerArray.length;
+//     // document.getElementById('player-list').innerText = playerArray.length;
     
-    displayPlayer(playerArray)
-}
+//     displayPlayer(playerArray)
+// }
---
+++ js/selection.js
@@ -0,0 +1,8 @@
+
+document.getElementsByClassName("select").addEventListener('click', function name(params) {
+    console.log('clicked')
+
+})
+
+
+
